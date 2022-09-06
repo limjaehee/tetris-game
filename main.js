ae7e671c00cdf405f79f454142a0baf5e67b258f
jaehee <rhak39@gmail.com>
2022-09-06 23:31:03 +0900

    UI 구성

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const canvas = document.getElementById("board");
+const rightArea = document.querySelector(".right-area");
+const ctx = canvas.getContext("2d");
 
-console.log(canvas);
+//캔버스의 크기 계산
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+rightArea.style.height = ROWS * BLOCK_SIZE + "px";
+
+//블록의 크기 변경
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
